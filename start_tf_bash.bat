aec8a128381cc197eec29676f286403536527ad6
sskram <ssk.ram@gmail.com>
2023-05-03 20:24:40 +0530

    interim commit - training code for nissl

--- start_tf_bash.bat
+++ start_tf_bash.bat
@@ -1,2 +1,2 @@
 #! /bin/bash
-docker run -it --ipc=host --gpus=all -p 9999:9999 -v c:/users/keerthi/code/hbp/samik_code/bhumika_maskrcnn_nissl:/workspace 73be11373498 bash
+docker run -it --ipc=host --gpus=all -p 9999:9999 -v c:/users/keerthi/code/hbp/samik_code/bhumika_maskrcnn_nissl:/workspace  -v c:/users/keerthi/documents/work/htic/hbp/data:/data 73be11373498 bash
